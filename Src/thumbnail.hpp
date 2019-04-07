88553437a473ed0c6b4104da454f2726a6eaa930
Shubham Anand <shubham_chess@live.com>
2019-04-07 17:20:01 +0530

    create read and write image functions

--- Src/thumbnail.hpp
+++ Src/thumbnail.hpp
@@ -3,6 +3,8 @@
 
 #include "image.hpp"
 
+#include <iostream>
+
 class apThumbnail {
 public:
     apThumbnail();
@@ -19,5 +21,37 @@ private:
     apImage image_;
     apImage thumbail_;
 };
+
+apThumbnail::apThumbnail(){}
+apThumbnail::~apThumbnail(){}
+
+void apThumbnail::readImage(const char *inputFile) {
+    image_ = apImage(64, 64);
+    try {
+        for (int y=0; y<image_.height(); y++) {
+            for (int x=0; x<image_.width(); x++) {
+                image_.setPixel(x, y, (unsigned char)(y % 255));
+            }
+        }
+    } catch (apImage::RangeError) {
+        throw Invalid();
+    }
+}
+
+void apThumbnail::writeImage(const char* outputFile) const {
+    // Save formatting state of stream
+    std::ios_base::fmtflags flags = std::cout.flags(std::cout.hex);
+    int width = std::cout.width(2);
+
+    for (int y=0; y<thumbail_.height(); y++) {
+        for (int x=0; x<thumbail_.width(); x++) {
+            std::cout << (int) thumbail_.getPixel(x, y) << " ";
+        }
+        std::cout << "\n";
+    }
+
+    std::cout.flags(flags);
+    std::cout.width(width);
+}
 
 #endif
