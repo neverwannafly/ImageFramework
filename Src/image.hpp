5b68bf49a3becb706761a932b3756404f1b1b4b7
Shubham Anand <shubham_chess@live.com>
2019-04-07 16:54:54 +0530

    add function definitions

--- Src/image.hpp
+++ Src/image.hpp
@@ -1,6 +1,8 @@
 #ifndef image_hpp
 #define image_hpp
 
+#include <cstring>
+
 class apImage {
 public:
     apImage();
@@ -31,5 +33,46 @@ private:
     unsigned char *pixels_;
 
 };
+
+void apImage::cleanup() {
+    delete [] pixels_;
+    width_ = 0;
+    height_ = 0;
+    pixels_ = 0;
+}
+
+void apImage::init() {
+    if (width_ > 0 && height_ > 0) {
+        pixels_ = new unsigned char[width_ * height_];
+    }
+}
+
+apImage::apImage(): width_(0), height_(0), pixels_(0) {};
+apImage::apImage(int width, int height): width_(width), height_(height), pixels_(0) {
+    init();
+}
+apImage::~apImage() {
+    cleanup();
+}
+
+apImage::apImage(const apImage &src) : width_(0), height_(0), pixels_(0) {
+    if (src.isValid()) {
+        width_ = src.width();
+        height_ = src.height();
+        init();
+        memcpy(pixels_, src.pixels_, width_*height_);
+    }
+}
+
+apImage &apImage::operator=(const apImage &src) {
+    if (&src != this) {
+        cleanup();
+        width_ = src.width();
+        height_ = src.height();
+        init();
+        memcpy(pixels_, src.pixels_, width_*height_);
+    }
+    return *this;
+}
 
 #endif
